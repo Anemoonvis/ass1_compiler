eb3bd76636b7f484a1d63d8644c0d161256a8d41
Thijs Leon Vromen <tlvromen@gmail.com>
2024-02-15 15:03:50 +0100

    1.5 basic

--- src/framework_assignments/collect_information.c
+++ src/framework_assignments/collect_information.c
@@ -22,8 +22,15 @@ void CIfini() { return; }
  */
 node_st *CIprogram(node_st *node)
 {
+    struct data_ci *data = DATA_CI_GET();
+    printf("%d", data->add);
     TRAVchildren(node);
-    
+    data = DATA_CI_GET();
+    printf("%d", data->add);
+    printf("%d", data->mul);
+    printf("%d", data->div);
+    printf("%d", data->mod);
+    printf("%d", data->sub);
 
     return node;
 }
@@ -34,6 +41,26 @@ node_st *CIprogram(node_st *node)
 node_st *CIbinop(node_st *node)
 {
     TRAVchildren(node);
+    struct data_ci *data = DATA_CI_GET();
+    switch (BINOP_OP(node)){
+        case BO_add:
+            data->add += 1;
+            break;
+        case BO_mul:
+            data->mul += 1;
+            break;
+        case BO_div:
+            data->div += 1;
+            break;
+        case BO_mod:
+            data->mod += 1;
+            break;
+        case BO_sub:
+            data->sub += 1; 
+            break;
+        default:
+            break;
+    }
     return node;
 }
 
